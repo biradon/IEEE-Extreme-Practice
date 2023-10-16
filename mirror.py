0b5790b8fe64d4fb469b1e054bbd919f2f4b6062
Minh Truong <61044654+biradon@users.noreply.github.com>
2023-10-16 10:34:32 -0400

    Reupload
    better version using Sieve of Eratosthenes

--- mirror.py
+++ mirror.py
@@ -1,31 +1,32 @@
 looptimes = int(input(""))
-# make sure it's less than or equal to 10
+final_final = []
+# # make sure it's less than or equal to 10
 if looptimes <= 10 and isinstance(looptimes, int):
-    final_result = []
     # Number of line will be ask user to input
     for j in range(0, looptimes):
+        final_result = []
         # user input in 1st line
         user_input = input("")
         # Take user input in the same line
         numbers = user_input.split()
         num1 = int(numbers[0])
         num2 = int(numbers[1])
-        prime_numbers = []
         # Check is the prime number or not
-        if 0 < num1 < 500 and 0 < num2 < 500:
-            for i in range (num1, num2):
-                if i == 2:
-                    prime_numbers.append(i)
-                if i == 3:
-                    prime_numbers.append(i)
-                if i == 5:
-                    prime_numbers.append(i)
-                if i == 7:
-                    prime_numbers.append(i)
-                if i % 2 != 0 and i % 3 != 0 and i % 5 != 0 and i % 7 != 0 and i != 1:
-                    prime_numbers.append(i)
+        if 0 < num1 <= 500 and 0 < num2 <= 500:
+            # Apply Sieve of Eratosthenes
+            prime_numbers = [True] * (num2+1)
+            prime_numbers[0] = prime_numbers[1] = False
+            p = 2
+            while p * p < num2:
+                if prime_numbers[p]:
+                    for i in range(p*p, num2+1, p):
+                        prime_numbers[i] = False
+                p += 1
             # Add prime numbers into the list
-            final_result.append(prime_numbers)
-        # Print the results
-    for m in range(0, len(final_result)):
-        print(len(final_result[m]))
+            for k in range(num1, num2+1):
+                if prime_numbers[k]:
+                    final_result.append(k)
+        final_final.append(final_result)
+# Print the results
+for m in range(0, looptimes):
+    print(len(final_final[m]))
