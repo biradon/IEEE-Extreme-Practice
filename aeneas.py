cad07e1377f7abf58858d45a6812da066519755c
Minh Truong <61044654+biradon@users.noreply.github.com>
2023-10-23 00:54:24 -0400

    Almost there!!!

--- aeneas.py
+++ aeneas.py
@@ -33,8 +33,53 @@ def find_destination_point(degree, hypotenuse):
     destination_point.append(round(y, 2))
     return destination_point
 
+def find_distance(x1,y1,x2,y2):
+    distance = round(math.sqrt((x2-x1)**2 + (y2-y1)**2),2)
+    return distance
 
 distance = float(input())
-degree = float(input())
+char_and_degree = []
+points = []
+result = 0
+i = 0
+while i < 26:
+    user_input = list(input().split(' '))
+    add_to_list = {'char': user_input[0].upper(),
+                   'degree': float(user_input[1])
+                   }
+    char_and_degree.append(add_to_list)
+    i+= 1
 
-print(find_destination_point(degree, distance))
+
+paragraph = input().upper()
+
+for i in range(len(paragraph)):
+    # First character
+    if i == 0:
+        for j in range(len(char_and_degree)):
+            if paragraph[i] == char_and_degree[j]['char']:
+                point = find_destination_point(char_and_degree[j]['degree'], distance)
+                points.append(point)
+    # From the second character check duplicate
+    if i > 0:
+        # If the last number is the same with the current number, skip
+        if paragraph[i] == paragraph[i-1]:
+            continue
+        # If it's not the same calculate
+        else:
+            for j in range(len(char_and_degree)):
+                if paragraph[i] == char_and_degree[j]['char']:
+                    point = find_destination_point(char_and_degree[j]['degree'], distance)
+                    points.append(point)
+
+
+# Find the distance between 2 points
+for i in range(len(points)):
+    # If in the last point stop the loop
+    if i == len(points) - 1:
+        break
+    else:
+        result += find_distance(points[i][0], points[i][1], points[i+1][0], points[i+1][1])
+
+print(result)
+
